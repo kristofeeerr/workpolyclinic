d402211c007c44712ba22f2e2c3802e8d0e34fb7
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-06 23:41:44 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workGrandMouther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityPeople;
+            int fixedTime = 10;
+            int minutes = 60;
+            
+            Console.Write($" Введите количество человек:");
+            quantityPeople = Convert.ToInt32( Console.ReadLine());
+
+            int totalMinutes = quantityPeople * fixedTime;
+            int hoursBeforeVisit = totalMinutes / minutes;
+            int minutesBeforeVisit = totalMinutes % minutes; 
+
+            Console.WriteLine($"Вы отстоите в очереди {hoursBeforeVisit} часа и {minutesBeforeVisit} минут");
+        }
+    }
+}
